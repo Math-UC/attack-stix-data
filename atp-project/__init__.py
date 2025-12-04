e545924bb6393ade8db876e536371259ba78ea76
Matthew Tsi <matthewofoc@gmail.com>
2025-12-04 18:02:15 -0500

    add ability to import dataframes from main_dataframe

---
+++ atp-project/__init__.py
@@ -0,0 +1,17 @@
+from .main_dataframe import (
+    main_df,
+    tech_df,
+    tactics_df,
+    rel_df,
+    groups_df,
+    group_techniques_df,
+)
+
+__all__ = [
+    "main_df",
+    "tech_df",
+    "tactics_df",
+    "rel_df",
+    "groups_df",
+    "group_techniques_df",
+]
