452128bdc925aac45dd3aaa58b0ec208a42f9890
Matthew Tsi <matthewofoc@gmail.com>
2025-12-05 07:45:33 -0500

    add config file *forgot to include it earlier

---
+++ config.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+
+PROJECT_ROOT = Path(__file__).resolve().parent
+ENTERPRISE_ATTACK_DATA = PROJECT_ROOT / "enterprise-attack"
+MOBILE_ATTACK_DATA = PROJECT_ROOT / "mobile-attack"
+ICS_ATTACK_DATA = PROJECT_ROOT / "ics-attack"
+APT_ROOT = PROJECT_ROOT / "apt_project"
